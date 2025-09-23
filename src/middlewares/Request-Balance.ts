b7c46d476e61660d7b3df16dd338a669ed4c7fc0
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-09-22 22:41:23 -0300

    version 1.0.24

--- src/middlewares/Request-Balance.ts
+++ src/middlewares/Request-Balance.ts
@@ -1,6 +1,7 @@
 import { redisGet, redisSet } from "@/cache"
 import { Request, Response, NextFunction } from "express"
 
+// Redis
 function RequestBalance (resource: string, limit: number) {
   return async (request: Request, response: Response, next: NextFunction) => {
     const ip = request.headers["x-forwarded-for"] || request.socket.remoteAddress
@@ -17,4 +18,24 @@ function RequestBalance (resource: string, limit: number) {
   }
 }
 
-export { RequestBalance }
+const cache = new Map()
+
+// Nodejs
+function RequestBalandeNodejs (resource: string, limit: number) {
+  return async (request: Request, response: Response, next: NextFunction) => {
+    const ip = request.headers["x-forwarded-for"] || request.socket.remoteAddress
+    const key = `rate-limit-${resource}-${ip}`
+
+    const requestCount = Number((await cache.get(key)) || 0) + 1
+    cache.set(key, requestCount)
+    setTimeout(() => cache.delete(key), 0.3 * 60 * 1000)
+
+    if (requestCount > limit) {
+      return response.status(400).json({ error: "rate-limit" })
+    }
+
+    next()
+  }
+}
+
+export { RequestBalance, RequestBalandeNodejs }
